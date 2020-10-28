6dfa09e3883dafe5b0867133b41d03a095f5cdae
djw319 <64276595+djw319@users.noreply.github.com>
2020-10-28 10:15:22 -0400

    Add accel XY Plot
    Simple plot that plots the xy values from the accel array

--- Blackbox_Reader.m
+++ Blackbox_Reader.m
@@ -11,3 +11,5 @@ time = test_file(1:end,1);          %Time (ms)
 accel = test_file(1:end,2:4);       %AccelX,AccelY,AccelZ (g's)
 gyro = test_file(1:end,5:7);        %GyroX,GyroY,GyroZ (dps)
 mag = test_file(1:end,8:10);        %MagX,MagY,MagZ (uT)
+
+plot(accel(:,1),accel(:,2));

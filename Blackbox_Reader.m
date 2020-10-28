76a468ff4318f7a83496e07f25c675652415d263
djw319 <64276595+djw319@users.noreply.github.com>
2020-10-28 10:04:43 -0400

    Reformat file name prompt

--- Blackbox_Reader.m
+++ Blackbox_Reader.m
@@ -3,7 +3,7 @@
 
 clear all; close all; clc;
 
-file_name = input('Please enter the MATLAB folder filepath','s');
+file_name = input('Please enter the MATLAB folder filepath: ','s');
 
 test_file = load(file_name);
 

016f8da388ed1ad2884a76339df945a8d90cd610
djw319 <64276595+djw319@users.noreply.github.com>
2020-10-28 10:09:01 -0400

    Add Array comments
    Adds comments to specify what is in the array and what units they are recorded in

--- Blackbox_Reader.m
+++ Blackbox_Reader.m
@@ -7,7 +7,7 @@ file_name = input('Please enter the MATLAB folder filepath: ','s');
 
 test_file = load(file_name);
 
-time = test_file(1:end,1);
-accel = test_file(1:end,2:4);
-gyro = test_file(1:end,5:7);
-mag = test_file(1:end,8:10);
+time = test_file(1:end,1);          %Time (ms)
+accel = test_file(1:end,2:4);       %AccelX,AccelY,AccelZ (g's)
+gyro = test_file(1:end,5:7);        %GyroX,GyroY,GyroZ (dps)
+mag = test_file(1:end,8:10);        %MagX,MagY,MagZ (uT)
